03ab328a71872b202ae1faac76bfbc4707bb06d3
zarish-creator <f2021266396@umt.edu.pk>
2025-08-06 15:59:22 +0500

    Fix: resolved merge conflict in User model

--- models/User.js
+++ models/User.js
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
-     const UserSchema = new mongoose.Schema({
-       username: {
-         type: String,
-         required: true,
-         unique: true,
-       },
-       password: {
-         type: String,
-         required: true,
-       },
-     }, { timestamps: true });
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
-=======
-const mongoose = require('mongoose');
-
-     const UserSchema = new mongoose.Schema({
-       username: {
-         type: String,
-         required: true,
-         unique: true,
-       },
-       password: {
-         type: String,
-         required: true,
-       },
-     }, { timestamps: true });
-
->>>>>>> b126bcc62967878e8aec81f6d5a034d6e2b4be01
-     module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
